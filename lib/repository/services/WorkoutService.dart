9f47848ee0ebee76de63d2d045069ce42edb5c6a
Tsames <tsames1@gmail.com>
2025-06-10 15:30:36 -0700

    Added update, delete, and clear table methods to our workout service.

--- lib/repository/services/WorkoutService.dart
+++ lib/repository/services/WorkoutService.dart
@@ -3,7 +3,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class WorkoutService {
-  static final _databaseName = "workouts";
+  static final _tableName = "workouts";
   static final WorkoutService instance = WorkoutService._init();
   static Database? _database;
 
@@ -17,25 +17,40 @@ class WorkoutService {
 
   Future<Database> _initDB() async {
     final dbPath = await getDatabasesPath();
-    final path = join(dbPath, _databaseName);
+    final path = join(dbPath, _tableName);
 
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
   Future<void> _createDB(Database db, int version) async {
     await db.execute(
-      'CREATE TABLE workouts(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, createdOn INTEGER NOT NULL)',
+      'CREATE TABLE $_tableName(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, createdOn INTEGER NOT NULL)',
     );
   }
 
   Future<List<Workout>> getWorkouts() async {
     final db = await instance.database;
-    final List<Map<String, Object?>> workoutMaps = await db.query(_databaseName);
+    final List<Map<String, Object?>> workoutMaps = await db.query(_tableName);
     return workoutMaps.map((map) => Workout.fromMap(map)).toList();
   }
 
   Future<void> insertWorkout(Workout workout) async {
     final db = await instance.database;
-    await db.insert(_databaseName, workout.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+    await db.insert(_tableName, workout.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> updateWorkout(Workout workout) async {
+    final db = await instance.database;
+    db.update(_tableName, workout.toMap(), where: 'id = ?', whereArgs: [workout.id]);
+  }
+
+  Future<void> deleteWorkout(int id) async {
+    final db = await instance.database;
+    await db.delete(_tableName, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<void> clearTable() async {
+    final db = await instance.database;
+    await db.delete(_tableName);
   }
 }
