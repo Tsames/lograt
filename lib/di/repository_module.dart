9e1d6217fa97dc545154757b90258a50b0f26ad0
Tsames <tsames1@gmail.com>
2025-06-16 14:49:06 -0700

    Using riverpods for dependency injection of the data and domain layers.

--- lib/di/repository_module.dart
+++
@@ -1,8 +0,0 @@
-import '../data/database/dao/workout_dao.dart';
-import '../data/repositories/workout_repository_impl.dart';
-import 'service_locator.dart';
-
-void setupRepositoryModule() {
-  // Register WorkoutRepository as a factory
-  serviceLocator.registerFactory<WorkoutRepository>(() => WorkoutRepository(serviceLocator<WorkoutDao>()));
-}
