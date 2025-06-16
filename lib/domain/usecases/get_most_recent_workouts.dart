0a3922ee3f33397a18ded7b2edd299b3b34f6938
Tsames <tsames1@gmail.com>
2025-06-16 14:01:27 -0700

    Added get most recent workouts use case.

---
+++ lib/domain/usecases/get_most_recent_workouts.dart
@@ -0,0 +1,18 @@
+import '../../domain/repository/workout_repository.dart';
+import '../../domain/entities/workout.dart';
+
+class GetMostRecentWorkouts {
+  final WorkoutRepository _repository;
+
+  GetMostRecentWorkouts(this._repository);
+
+  // This method encapsulates the business logic for getting recent workouts
+  // It can include validation, business rules, or data transformation
+  Future<List<Workout>> call() async {
+    final workouts = await _repository.getMostRecentWorkouts();
+
+    // Example business rule: only return workouts from the last 30 days
+    final thirtyDaysAgo = DateTime.now().subtract(const Duration(days: 30));
+    return workouts.where((workout) => workout.createdOn.isAfter(thirtyDaysAgo)).toList();
+  }
+}
