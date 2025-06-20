b3352acf8bccb8962ab535db65280de534910041
Tsames <tsames1@gmail.com>
2025-06-19 18:48:11 -0700

    Adjusted entities to be consistent and allow for nullable keys.

--- lib/domain/entities/exercise_type.dart
+++ lib/domain/entities/exercise_type.dart
@@ -1,9 +1,14 @@
+/// Represents a specific type of exercise such as 'Push ups' or 'Squats'
 class ExerciseType {
-  final int? id;
-  final String name;
+  final int? id; // SQLite generated primary key
+  final String name; // Name of the exercise
   final String? description;
 
   const ExerciseType({required this.id, required this.name, this.description});
+
+  ExerciseType copyWith({int? id, String? name, String? description}) {
+    return ExerciseType(id: id ?? this.id, name: name ?? this.name, description: description ?? this.description);
+  }
 
   @override
   bool operator ==(Object other) =>
@@ -13,5 +18,5 @@ class ExerciseType {
   int get hashCode => id.hashCode;
 
   @override
-  String toString() => name;
+  String toString() => 'ExerciseType{ id: ${id ?? 'null'}, name: $name }';
 }
