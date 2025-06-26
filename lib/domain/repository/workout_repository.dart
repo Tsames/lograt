8b70aa6ab91d460f8afd459071c095d93a90a60b
Tsames <tsames1@gmail.com>
2025-06-26 14:59:26 -0700

    Made additions to the workout repository interface

--- lib/domain/repository/workout_repository.dart
+++ lib/domain/repository/workout_repository.dart
@@ -1,9 +1,31 @@
+import 'package:lograt/domain/entities/exercise_type.dart';
+
 import '../../domain/entities/workout.dart';
+import '../entities/exercise.dart';
+import '../entities/exercise_set.dart';
+import '../entities/workout_summary.dart';
 
 abstract class WorkoutRepository {
-  Future<void> addWorkout(Workout workout);
-  Future<void> addWorkouts(List<Workout> workouts);
-  Future<List<Workout>> getMostRecentWorkouts();
+  Future<List<WorkoutSummary>> getMostRecentSummaries(int limit);
+  Future<Workout> getFullWorkoutDetails(int workoutId);
+  Future<List<Exercise>> getRecentExercisesOfType({ExerciseType type, int limit});
+
+  Future<int> createWorkout(Workout workout);
+  Future<int> createExercise(Exercise exercise);
+  Future<int> createExerciseType(ExerciseType type);
+  Future<int> createExerciseSet(ExerciseSet set);
+  Future<int> createWorkouts(List<Workout> workouts);
+
+  Future<void> updateWorkout({required int workoutId, required Workout updatedWorkout});
+  Future<void> updateExercise({required int exerciseId, required Exercise updatedExercise});
+  Future<void> updateExerciseType({required int typeId, required ExerciseType updatedType});
+  Future<void> updateExerciseSet({required int setId, required ExerciseSet updatedSet});
+
+  Future<void> deleteWorkout(int workoutId);
+  Future<void> deleteExercise(int exerciseId);
+  Future<void> deleteExerciseType(int typeId);
+  Future<void> deleteExerciseSet(int setId);
+
   Future<void> clearWorkouts();
   Future<void> seedWorkouts();
 }
