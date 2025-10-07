60df12f4feb8a296ff7e5b6daf9f0096113a7f7a
Tom Ames <Tsames@users.noreply.github.com>
2025-10-07 11:48:36 -0700

    added-get-all-exercise-types-method-to-repository (#55)

---
+++ app/lib/presentation/screens/log/exercise_tracker_state.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lograt/domain/entities/exercise.dart';
+
+import '../../../domain/entities/exercise_type.dart';
+import 'exercise_tracker.dart';
+
+class ExerciseTrackerState extends State<ExerciseTracker> {
+  late ExerciseType? selectedExerciseType = widget.exercise.exerciseType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+    // return Column(
+    //   crossAxisAlignment: CrossAxisAlignment.stretch,
+    //   children: [
+    //     // Exercise type dropdown
+    //     DropdownButtonFormField<ExerciseType>(
+    //       initialValue: selectedExerciseType,
+    //       decoration: const InputDecoration(labelText: 'Exercise', border: OutlineInputBorder()),
+    //       items: widget.availableExerciseTypes
+    //           .map((type) => DropdownMenuItem(value: type, child: Text(type.name)))
+    //           .toList(),
+    //       onChanged: (value) {
+    //         setState(() {
+    //           selectedExerciseType = value;
+    //         });
+    //       },
+    //     ),
+    //     const SizedBox(height: 16),
+    //   ],
+    // );
+  }
+}
