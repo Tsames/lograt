3898d310555053fb24c279d6defbc33828c735d6
Tsames <tsames1@gmail.com>
2025-10-27 10:11:23 -0700

    Changed home to 'this week' tab

---
+++ app/lib/presentation/screens/this_week/this_week_workout_history_provider.dart
@@ -0,0 +1,73 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lograt/domain/usecases/clear_workouts.dart';
+
+import '../../../di/providers.dart';
+import '../../../domain/entities/workout.dart';
+import '../../../domain/usecases/get_most_recent_workouts.dart';
+
+class ThisWeekWorkoutHistoryState {
+  final List<Workout> workoutsThisWeek;
+  final bool isLoading;
+  final String? error;
+
+  const ThisWeekWorkoutHistoryState({
+    this.workoutsThisWeek = const [],
+    this.isLoading = false,
+    this.error,
+  });
+
+  ThisWeekWorkoutHistoryState copyWith({
+    List<Workout>? workouts,
+    bool? isLoading,
+    String? error,
+  }) {
+    return ThisWeekWorkoutHistoryState(
+      workoutsThisWeek: workouts ?? workoutsThisWeek,
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+    );
+  }
+}
+
+class ThisWeekWorkoutHistoryNotifier
+    extends StateNotifier<ThisWeekWorkoutHistoryState> {
+  final GetMostRecentWorkouts _getMostRecentWorkouts;
+  final ClearWorkout _clearWorkouts;
+
+  ThisWeekWorkoutHistoryNotifier(
+    this._getMostRecentWorkouts,
+    this._clearWorkouts,
+  ) : super(const ThisWeekWorkoutHistoryState()) {
+    loadWorkouts();
+  }
+
+  Future<void> loadWorkouts() async {
+    state = state.copyWith(isLoading: true, error: null);
+
+    try {
+      final workouts = await _getMostRecentWorkouts();
+      state = state.copyWith(workouts: workouts, isLoading: false);
+    } catch (error) {
+      state = state.copyWith(isLoading: false, error: error.toString());
+    }
+  }
+
+  Future<void> clearWorkouts() async {
+    await _clearWorkouts();
+    state = state.copyWith(workouts: const []);
+  }
+}
+
+final thisWeekWorkoutHistoryProvider =
+    StateNotifierProvider<
+      ThisWeekWorkoutHistoryNotifier,
+      ThisWeekWorkoutHistoryState
+    >((ref) {
+      final getMostRecentWorkouts = ref.read(getMostRecentWorkoutsProvider);
+      final clearWorkoutList = ref.read(clearWorkoutProvider);
+
+      return ThisWeekWorkoutHistoryNotifier(
+        getMostRecentWorkouts,
+        clearWorkoutList,
+      );
+    });
