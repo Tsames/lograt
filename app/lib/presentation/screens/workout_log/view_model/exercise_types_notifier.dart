47e3bdf5c9498f4751f6650339ebf494d5bd32be
Tsames <tsames1@gmail.com>
2025-11-05 10:21:48 -0800

    Added view model for workout log screen

---
+++ app/lib/presentation/screens/workout_log/view_model/exercise_types_notifier.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lograt/data/providers.dart';
+import 'package:lograt/presentation/screens/workout_log/view_model/exercise_types_notifier_state.dart';
+
+import '../../../../data/usecases/get_paginated_exercise_types_usecase.dart';
+
+class ExerciseTypesNotifier extends StateNotifier<ExerciseTypesNotifierState> {
+  final GetPaginatedExerciseTypesUsecase _getExerciseTypesUsecase;
+
+  ExerciseTypesNotifier(this._getExerciseTypesUsecase)
+    : super(ExerciseTypesNotifierState()) {
+    loadExerciseTypes();
+  }
+
+  Future<void> loadExerciseTypes() async {
+    if (!state.hasMore) return;
+    state = state.copyWith(isLoading: true, error: null);
+    try {
+      final paginatedResults = await _getExerciseTypesUsecase(state.offset);
+      state = state.copyWith(
+        isLoading: false,
+        exerciseTypes: paginatedResults.results,
+        offset: paginatedResults.nextOffset,
+        hasMore: paginatedResults.hasMore,
+      );
+    } catch (error) {
+      state = state.copyWith(isLoading: false, error: error.toString());
+    }
+  }
+}
+
+final exerciseTypesProvider =
+    StateNotifierProvider<ExerciseTypesNotifier, ExerciseTypesNotifierState>((
+      ref,
+    ) {
+      final getExerciseTypesUsecase = ref.read(getExerciseTypesUsecaseProvider);
+      return ExerciseTypesNotifier(getExerciseTypesUsecase);
+    });
--- app/lib/presentation/screens/workout_log/view_model/workout_log_notifier.dart
+++ app/lib/presentation/screens/workout_log/view_model/workout_log_notifier.dart
@@ -1,34 +1,42 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lograt/data/providers.dart';
+import 'package:lograt/data/usecases/get_full_workout_data_by_id_usecase.dart';
 import 'package:lograt/presentation/screens/workout_log/view_model/workout_log_notifier_state.dart';
 
 import '../../../../data/entities/workout.dart';
-import '../../../../data/usecases/get_all_exercise_types_usecase.dart';
+import '../../../../data/providers.dart';
 
 class WorkoutLogNotifier extends StateNotifier<WorkoutLogNotifierState> {
-  final GetExerciseTypesUsecase _getExerciseTypesUsecase;
+  final GetFullWorkoutDataByIdUsecase _getFullWorkoutDataByIdUsecase;
 
-  WorkoutLogNotifier(this._getExerciseTypesUsecase, Workout workout)
+  WorkoutLogNotifier(this._getFullWorkoutDataByIdUsecase, Workout workout)
     : super(WorkoutLogNotifierState(workout: workout)) {
-    loadExerciseTypes();
+    loadFullWorkoutData();
   }
 
-  WorkoutLogNotifier.empty(GetExerciseTypesUsecase getExerciseTypesUsecase)
-    : this(getExerciseTypesUsecase, Workout.empty());
-
-  Future<void> loadExerciseTypes() async {
+  Future<void> loadFullWorkoutData() async {
+    if (state.workout.id == null) {
+      return;
+    }
     state = state.copyWith(isLoading: true, error: null);
     try {
-      final exerciseTypes = await _getExerciseTypesUsecase();
-      state = state.copyWith(isLoading: false, exerciseTypes: exerciseTypes);
-    } catch (error) {
+      final fullWorkoutData = await _getFullWorkoutDataByIdUsecase(
+        state.workout.id!,
+      );
+      state = state.copyWith(isLoading: false, workout: fullWorkoutData);
+    } catch (stacktrace, error) {
       state = state.copyWith(isLoading: false, error: error.toString());
+      rethrow;
     }
   }
 }
 
-final workoutLogProvider =
-    StateNotifierProvider<WorkoutLogNotifier, WorkoutLogNotifierState>((ref) {
-      final getExerciseTypesUsecase = ref.read(getExerciseTypesUsecaseProvider);
-      return WorkoutLogNotifier.empty(getExerciseTypesUsecase);
+final workoutLogProvider = StateNotifierProvider.autoDispose
+    .family<WorkoutLogNotifier, WorkoutLogNotifierState, Workout>((
+      ref,
+      Workout workout,
+    ) {
+      final getFullWorkoutDataByIdUsecase = ref.read(
+        getFullWorkoutDataByIdUsecaseProvider,
+      );
+      return WorkoutLogNotifier(getFullWorkoutDataByIdUsecase, workout);
     });
