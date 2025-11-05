d62cd6896fc827ec166f294bb1e0a54b2edb79fb
Tom Ames <Tsames@users.noreply.github.com>
2025-11-05 10:10:58 -0800

    Updated exercise types repository method and usecase (#84)

---
+++ app/lib/data/usecases/get_paginated_exercise_types_usecase.dart
@@ -0,0 +1,22 @@
+import '../../util/paginated_results.dart';
+import '../entities/exercise_type.dart';
+import '../repositories/workout_repository.dart';
+
+class GetPaginatedExerciseTypesUsecase {
+  static const pageSize = 50;
+  final WorkoutRepository _repository;
+
+  GetPaginatedExerciseTypesUsecase(this._repository);
+
+  Future<PaginatedResults<List<ExerciseType>>> call(int? offset) async {
+    final paginatedExerciseTypes = await _repository.getExerciseTypes(
+      limit: pageSize,
+      offset: offset,
+    );
+    return PaginatedResults<List<ExerciseType>>(
+      paginatedExerciseTypes,
+      (offset ?? 0) + pageSize,
+      paginatedExerciseTypes.length == pageSize,
+    );
+  }
+}
