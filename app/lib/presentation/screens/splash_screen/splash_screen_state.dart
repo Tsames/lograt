208b63514473b8c8b77c77709fad02aad14807c2
Tom Ames <Tsames@users.noreply.github.com>
2025-11-23 16:39:37 -0800

    Added flutter_svg to project and using a custom svg for the splash screen now (#120)

--- app/lib/presentation/screens/splash_screen/splash_screen_state.dart
+++ app/lib/presentation/screens/splash_screen/splash_screen_state.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lograt/app.dart';
 import 'package:lograt/presentation/screens/splash_screen/splash_screen_widget.dart';
 
@@ -25,7 +26,14 @@ class SplashScreenState extends State<SplashScreenWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Center(child: Image(image: AssetImage('assets/lograt_icon.png'))),
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/icons/lograt_icon.svg',
+          semanticsLabel: 'Lograt Icon',
+          width: 300,
+          height: 300,
+        ),
+      ),
     );
   }
 }
