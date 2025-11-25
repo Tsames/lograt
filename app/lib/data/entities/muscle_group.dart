500e3a0cc4a26f727a83aec967e95075fcb3500c
Tom Ames <Tsames@users.noreply.github.com>
2025-11-25 12:34:31 -0800

    Added muscle groups entity (#127)

---
+++ app/lib/data/entities/muscle_group.dart
@@ -0,0 +1,20 @@
+import 'package:collection/collection.dart';
+
+enum MuscleGroup {
+  chest('Chest'),
+  shoulders('Shoulders'),
+  back('Back'),
+  arms('Arms'),
+  abs('Abs'),
+  legs('Legs');
+
+  final String name;
+
+  const MuscleGroup(this.name);
+
+  static MuscleGroup? fromString(String value) {
+    return MuscleGroup.values.firstWhereOrNull(
+      (muscleGroup) => muscleGroup.name == value,
+    );
+  }
+}
