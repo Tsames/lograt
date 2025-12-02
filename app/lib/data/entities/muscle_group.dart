09dee499fabd8e924af0cc034a0c040fe3ed8840
Tsames <tsames1@gmail.com>
2025-12-02 10:02:57 -0800

    Added muscle groups entity and model

--- app/lib/data/entities/muscle_group.dart
+++ app/lib/data/entities/muscle_group.dart
@@ -1,20 +1,31 @@
-import 'package:collection/collection.dart';
+import 'package:lograt/util/uuidv7.dart';
 
-enum MuscleGroup {
-  chest('Chest'),
-  shoulders('Shoulders'),
-  back('Back'),
-  arms('Arms'),
-  core('Core'),
-  legs('Legs');
+class MuscleGroup {
+  final String id;
+  final String label;
+  final String? description;
 
-  final String name;
+  MuscleGroup({String? id, required this.label, this.description})
+    : id = id ?? uuidV7();
 
-  const MuscleGroup(this.name);
-
-  static MuscleGroup? fromString(String value) {
-    return MuscleGroup.values.firstWhereOrNull(
-      (muscleGroup) => muscleGroup.name == value,
+  MuscleGroup copyWith({String? id, String? label, String? description}) {
+    return MuscleGroup(
+      id: id ?? this.id,
+      label: label ?? this.label,
+      description: description ?? this.description,
     );
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is MuscleGroup && id == other.id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  String toString() =>
+      'MuscleGroup(id: $id, label: $label, description: $description)';
 }
