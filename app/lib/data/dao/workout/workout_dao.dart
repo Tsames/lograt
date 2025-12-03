128e15aba6678abcd3e61f1a7ac6d7872d54f606
Tsames <tsames1@gmail.com>
2025-12-02 17:29:24 -0800

    Updated workout dao and tests

--- app/lib/data/dao/workout/workout_dao.dart
+++ app/lib/data/dao/workout/workout_dao.dart
@@ -21,7 +21,7 @@ class WorkoutDao {
 
   /// Get a list of the workouts of length [limit] starting from [offset]
   /// Workouts returned will be in order of creation date DESC
-  Future<List<WorkoutModel>> getListOfWorkoutsOrderedByCreationDate({
+  Future<List<WorkoutModel>> getAllPaginatedOrderedByCreationDate({
     int? limit,
     int? offset,
     Transaction? txn,
@@ -47,23 +47,88 @@ class WorkoutDao {
     );
   }
 
-  Future<int> update(WorkoutModel workout, [Transaction? txn]) async {
+  Future<void> batchInsert(
+    List<WorkoutModel> workouts, [
+    Transaction? txn,
+  ]) async {
+    if (workouts.isEmpty) return;
+
     final DatabaseExecutor executor = txn ?? await _db.database;
-    return await executor.update(
+    final batch = executor.batch();
+
+    for (final workout in workouts) {
+      batch.insert(
+        workoutsTable,
+        workout.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.fail,
+      );
+    }
+
+    await batch.commit(noResult: true);
+  }
+
+  Future<void> update(WorkoutModel workout, [Transaction? txn]) async {
+    final DatabaseExecutor executor = txn ?? await _db.database;
+    final rowsUpdated = await executor.update(
       workoutsTable,
       workout.toMap(),
       where: '${WorkoutFields.id} = ?',
       whereArgs: [workout.id],
     );
+
+    if (rowsUpdated == 0) {
+      throw Exception('Cannot update workout $workout: does not exist');
+    }
   }
 
-  Future<int> delete(String id, [Transaction? txn]) async {
+  Future<void> batchUpdate(
+    List<WorkoutModel> workouts, [
+    Transaction? txn,
+  ]) async {
+    if (workouts.isEmpty) return;
+
+    Future<void> executeUpdate(Transaction transaction) async {
+      for (final workout in workouts) {
+        final exists = await getById(workout.id, transaction);
+        if (exists == null) {
+          throw Exception('Cannot update workout $workout: does not exist');
+        }
+      }
+
+      final batch = transaction.batch();
+      for (final workout in workouts) {
+        batch.update(
+          workoutsTable,
+          workout.toMap(),
+          where: '${WorkoutFields.id} = ?',
+          whereArgs: [workout.id],
+        );
+      }
+      await batch.commit(noResult: true);
+    }
+
+    // If no transaction provided, create one
+    if (txn != null) {
+      await executeUpdate(txn);
+    } else {
+      final db = await _db.database;
+      await db.transaction((transaction) async {
+        await executeUpdate(transaction);
+      });
+    }
+  }
+
+  Future<void> delete(String id, [Transaction? txn]) async {
     final DatabaseExecutor executor = txn ?? await _db.database;
-    return await executor.delete(
+    final rowsDeleted = await executor.delete(
       workoutsTable,
       where: '${WorkoutFields.id} = ?',
       whereArgs: [id],
     );
+
+    if (rowsDeleted == 0) {
+      throw Exception('Cannot delete workout $id: does not exist');
+    }
   }
 
   Future<void> clearTable([Transaction? txn]) async {
